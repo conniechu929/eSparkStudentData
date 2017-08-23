35c296a88e02ce3d63a3efc618368692b7b5b8ed
conniechu929 <conniechu929@gmail.com>
2017-08-22 18:57:43 -0700

    finished adding student paths

--- src/components/student_test.js
+++ src/components/student_test.js
@@ -5,6 +5,7 @@ import { parsedStudentData } from '../actions';
 import { csv } from 'd3-request';
 import * as d3 from "d3";
 import { csvParseRows } from 'd3-dsv';
+import update from 'immutability-helper';
 
 class Node {
     constructor(val, priority) {
@@ -21,12 +22,12 @@ class PriorityQueue {
 
     insert(value, priority) {
       const newNode = new Node(value, priority);
-      if (!this.first || priority > this.first.priority) {
+      if (!this.first || priority < this.first.priority) {
         newNode.next = this.first;
         this.first = newNode;
       } else {
         let pointer = this.first;
-        while (pointer.next && priority < pointer.next.priority) {
+        while (pointer.next && priority >= pointer.next.priority) {
           pointer = pointer.next;
         }
         newNode.next = pointer.next;
@@ -34,23 +35,19 @@ class PriorityQueue {
       }
     }
 
-    parseDomain() {
-      const obj = {}
-        d3.text('./src/data/domain_order1.csv', function(text) {
-          d3.csvParseRows(text).map(function(row) {
-              return row.map(function(value) {
-                return obj[row[0]] = Array.from(row.slice(1));
-              })
-          });
-        })
-        return obj
+  showQueue() {
+    let current = this.first;
+    while(current) {
+      if (current.priority === 0) {
+        var curr_priority = "K"
+        current.priority = curr_priority
+      }
+      console.log("Level: ", current.value, "TEST: ", current.priority);
+      current = current.next;
     }
+  }
 
 }
-
-// const test_domains = PriorityQueue.parseDomain;
-// console.log("DOMAIN OBJECT IN QUEUE: ", test_domains);
-console.log("IN THE QUEUE")
 
 class StudentsIndex extends Component {
   constructor(props) {
@@ -65,75 +62,83 @@ class StudentsIndex extends Component {
        }
       this.setState({
         data: _.mapKeys(data, 'Student Name')
-        // data: _.mapValues(data, "Student Name")
       })
     })
 
-    this.parseDomain();
-    console.log("THIS IS THE DOMAIN OBJ: ", this.parseDomain())
-    // this.studentPaths();
   }
 
   parseDomain() {
-    const obj = {}
-      d3.text('./src/data/domain_order1.csv', function(text) {
-        d3.csvParseRows(text).map(function(row) {
-            return row.map(function(value) {
-              return obj[row[0]] = Array.from(row.slice(1));
+      const DomainQueue = new PriorityQueue()
+      let promise = new Promise((resolve) => {
+        d3.text('./src/data/domain_order1.csv', function(text) {
+          d3.csvParseRows(text).map(function(row) {
+                if(row[0] === "K"){
+                  var curr_priority = 0
+                }
+                else {
+                  var curr_priority = row[0]
+                }
+                row.slice(1).map(function(value) {
+                  return DomainQueue.insert(value, curr_priority)
+                })
             })
-        });
+            resolve(DomainQueue)
+          })
       })
-      return obj
+      return promise
   }
 
-
-
-  studentPaths() {
-    const grade_domain = this.parseDomain();
-    // var domain_array = []
-    // Object.keys(grade_domain).forEach(function(key) {
-    //   grade
-    // });
-    // console.log('DOMAIN_ARRAY:', domain_array);
-    // console.log('THIS IS THE GRADE DOMAIN OBJ: ', grade_domain)
-    console.log("STATE IN STUDENT PATHS: ", this.state.data)
-    const studentData = this.state.data;
-    _.map(studentData, student => {
-        console.log(student)
-            var studentKey = Object.keys(student);
-            var lowest = _.min(studentKey, function (val) {
-              var first_min = 0
-              if(student[val] === "K" || student[val] === 'k'){
-                console.log("IN IF CHECK")
-                return first_min
-              }
-              else if(student[val] === '' || student[val] === null){
-                return -1
-              }
-              else {
-                first_min = parseInt(student[val]);
-                console.log("FIRST MIN: ", first_min)
-                return first_min
-              }
-          });
-          console.log("LOWEST GRADE: ", student[lowest])
-          console.log("LOWEST DOMAIN: ", lowest)
-          return lowest;
-    })
-    // find the min of the grades from studentTest
-    // while loop --> while count is less than 5
-      // loop from the lowest test in domain_order
-      // compare the key(which is the grade) to the grade associated to the test of the student
-      // if grade of student is less than the grade at the domain, add test to Array
-      // return 5 total tests
-    var count = 0;
-    while (count < 5) {
-
-      count++
+  convertDomainLevel(domain){
+    if(domain.priority === 0) {
+      return "K"
+    }
+    else {
+      return domain.priority.toString()
     }
   }
 
+  studentPaths() {
+    const studentData = this.state.data;
+    let new_data = {}
+    const grade_domain = this.parseDomain().then((DomainQueue) => {
+      _.map(studentData, student => {
+        var current_domain = DomainQueue.first
+
+        while(current_domain) {
+          var current_level = this.convertDomainLevel(current_domain)
+          if(student[current_domain.value] === current_level || student[current_domain.value]  === " ") {
+            // console.log("MINIMUM VALUE. STUDENT NAME: ", student["Student Name"], " Level: ", current_level, " Test: ", current_domain.value)
+            var count = 1;
+            var path = [current_level +"."+ current_domain.value]
+            current_domain = current_domain.next;
+            while(count < 5 && current_domain) {
+              var current_level = this.convertDomainLevel(current_domain)
+              if(current_level !== student[current_domain.value]) {
+                path.push(current_level +"."+ current_domain.value)
+                count++;
+              }
+              else {
+                current_domain = current_domain.next;
+              }
+            }
+            path = path.join(', ');
+            student.path = path
+
+            // console.log("STUDENT NAME FROM STATE: ", this.state.data[student['Student Name']]);
+          }
+          current_domain = current_domain.next;
+        }
+        new_data[student['Student Name']] = student;
+        // this.setState({...this.state.data, student: student.path });
+      })
+      if(new_data != this.state.data){
+        this.setState({data: new_data});
+      }
+    });
+  }
+
   renderStudents() {
+    this.studentPaths()
     return _.map(this.state.data, d => {
       if (d["Student Name"] != null) {
         return (
@@ -143,6 +148,7 @@ class StudentsIndex extends Component {
             <p>Reading Literature: {d["RL"]}</p>
             <p>Reading Informational Text: {d["RI"]}</p>
             <p>Level: {d["L"]}</p>
+            <p>Path: {d["path"]} </p>
           </li>
           )
         }
@@ -161,7 +167,7 @@ class StudentsIndex extends Component {
         <h3>Student Paths</h3>
         <ul className="list-group">
           {this.renderStudents()}
-          {this.studentPaths()}
+
         </ul>
       </div>
     );
